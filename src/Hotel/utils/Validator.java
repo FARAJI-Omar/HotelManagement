79c839ae9310439e9572c130fcc6ee2930a31f7f
F99 <farajiomar99@gmail.com>
2025-09-20 14:24:59 +0100

    feat: add validation methods for hotel total rooms and duplicate hotel names

--- src/Hotel/utils/Validator.java
+++ src/Hotel/utils/Validator.java
@@ -1,5 +1,10 @@
 package Hotel.utils;
 
+import Hotel.entity.Hotel;
+import Hotel.repository.HotelRepository;
+import java.util.List;
+import java.util.UUID;
+
 public class Validator {
     // name cant be empty
     public static boolean isValidName(String name) {
@@ -15,4 +20,21 @@ public class Validator {
     public static boolean isValidPassword(String password) {
         return password != null && password.length() >= 6;
     }
+
+    // hotel total rooms must be positive
+    public static boolean isValidTotalRooms(int totalRooms) {
+        return totalRooms > 0;
+    }
+
+    // don't allow hotel duplicate names
+    public static boolean hotelNotExists(UUID ownerId, String hotelName, HotelRepository hotelRepository) {
+        List<Hotel> hotels = hotelRepository.findAll(ownerId);
+        for (Hotel hotel : hotels) {
+            if (hotel.getHotelName().equalsIgnoreCase(hotelName)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
