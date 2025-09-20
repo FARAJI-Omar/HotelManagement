0f7e40d18e36db7cb2aabaa06b6b5aa667929f16
F99 <farajiomar99@gmail.com>
2025-09-20 14:44:14 +0100

    feat: implement create and update methods in HotelServiceImpl with validation

--- src/Hotel/service/HotelService.java
+++ src/Hotel/service/HotelService.java
@@ -1,5 +1,28 @@
 package Hotel.service;
 
-public interface HotelService {
+import Hotel.entity.Hotel;
 
+import java.util.List;
+import java.util.UUID;
+
+public interface HotelService {
+    void createHotel(UUID ownerId, String hotelName, String hotelAddress, int totalRooms);
+
+//    // READ
+//    Optional<Hotel> findById(UUID hotelID);
+//
+//    Optional<Hotel> findByName(String hotelName);
+
+    List<Hotel> findAllHotels(UUID ownerId);
+
+//    // UPDATE
+    void update(UUID ownerId, String originalHotelName, String newHotelName, String newHotelAddress, int newTotalRooms);
+//
+//    void updateAvailability(String hotelName, int availableRooms);
+//
+//    // DELETE
+//    void delete(String hotelName);
+//
+//    // RATING
+//    void rate(String hotelName, double newRating);
 }
--- src/Hotel/service/serviceImp/HotelServiceImpl.java
+++ src/Hotel/service/serviceImp/HotelServiceImpl.java
@@ -1,7 +1,49 @@
 package Hotel.service.serviceImp;
 
+import Hotel.entity.Hotel;
 import Hotel.service.HotelService;
+import Hotel.repository.HotelRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+import static Hotel.utils.Validator.hotelNotExists;
 
 public class HotelServiceImpl implements HotelService {
+    private final HotelRepository hotelRepo;
 
+    public HotelServiceImpl(HotelRepository hotelRepository){
+        this.hotelRepo = hotelRepository;
+    }
+
+    //create hotel
+    @Override
+    public void createHotel(UUID hotelOwner, String hotelName, String hotelAddress, int totalRooms) {
+        if (hotelNotExists(hotelOwner, hotelName, hotelRepo)) {
+            hotelRepo.save(hotelOwner, hotelName, hotelAddress, totalRooms);
+            System.out.println("Hotel created successfully.");
+        } else {
+            System.out.println("Hotel " + hotelName + " already exists.");
+        }
+    }
+
+    //update hotel
+    @Override
+    public void update(UUID ownerId, String originalHotelName, String hotelName, String hotelAddress, int totalRooms) {
+        if (hotelNotExists(ownerId, originalHotelName, hotelRepo)) {
+            System.out.println("Hotel: " + originalHotelName + " does not exist.");
+        } else {
+            hotelRepo.update(originalHotelName, hotelName, hotelAddress, totalRooms);
+            System.out.println("Hotel updated successfully!");
+        }
+    }
+
+
+
+
+    //list all hotels
+    @Override
+    public List<Hotel> findAllHotels(UUID ownerId) {
+        return hotelRepo.findAll(ownerId);
+    }
 }
